7a0c5c9de687dca95f2d67dd659f20a61479426d
Kostiantyn Rozov <kostiantyn.rozov@spritecloud.com>
2020-04-05 18:15:03 +0300

    made tests independant and added after method

--- src/test/java/GetPetTest.java
+++ src/test/java/GetPetTest.java
@@ -1,9 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class GetPetTest {
 
@@ -18,7 +19,35 @@ public class GetPetTest {
 
     @Test
     public void getPetById(){
+        String name = "sammy";
         int petId = 999888;
+        given()
+                .body("{\n" +
+                        "  \"id\": " + petId + ",\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"sammy\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .when()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .body("name", is("sammy"))
+                .statusCode(200);
+
         given()
                 .log()
                 .all()
@@ -40,7 +69,7 @@ public class GetPetTest {
                 .then()
                 .log()
                 .all()
-                .body("status[0]", is("sold"))
+                .body("status", everyItem(equalTo(status)))
                 .statusCode(200);
     }
     @Test
@@ -78,6 +107,33 @@ public class GetPetTest {
     public void updatePet() {
         int petId = 999888;
         given()
+                .body("{\n" +
+                        "  \"id\": " + petId + ",\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"sammy\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .when()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .body("name", is("sammy"))
+                .statusCode(200);
+
+        given()
                 .log()
                 .all()
                 .contentType("application/x-www-form-urlencoded")
@@ -93,9 +149,35 @@ public class GetPetTest {
     }
     @Test
     public void updateExistingPet() {
-        //#TO DO: precondition - create a pet if it doesn't exist
-        String name = "annet";
         int petId = 999888;
+        given()
+                .body("{\n" +
+                        "  \"id\": " + petId + ",\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"sammy\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .when()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .body("name", is("sammy"))
+                .statusCode(200);
+
+        String name = "annet";
         given()
                 .body("{\n" +
                         "  \"id\": " + petId + " ,\n" +
@@ -125,17 +207,47 @@ public class GetPetTest {
     }
     @Test
     public void deletePet() {
-        //#TO DO: precondition - create a pet if it doesn't exist
-            int petId = 999888;
-            given()
-                    .header("api_key", "special-key")
-                    .when()
-                    .delete("/pet/{petId}", petId)
-                    .then()
-                    .log()
-                    .all()
-                    .body("message", is(String.valueOf(petId)))
-                    .statusCode(200);
+        int petId = 999888;
+        given()
+        .body("{\n" +
+                "  \"id\": " + petId + ",\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"sammy\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}")
+                .when()
+                .post("/pet")
+                .then()
+                .log()
+                .all()
+                .body("name", is("sammy"))
+                .statusCode(200);
+
+        given()
+                .header("api_key", "special-key")
+                .when()
+                .delete("/pet/{petId}", petId)
+                .then()
+                .log()
+                .all()
+                .body("message", is(String.valueOf(petId)))
+                .statusCode(200);
+    }
+    @After
+    public void after() {
+        RestAssured.reset();
     }
 }
 
