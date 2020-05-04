89889ff80196c1cc6c22c1de2396b22d206bc766
Kostiantyn Rozov <kostiantyn.rozov@spritecloud.com>
2020-05-04 16:14:06 +0300

    updated uploadImageTest and GetPetByStatusTest with Serenity data provider

--- src/test/java/GetPetByStatusTest.java
+++ src/test/java/GetPetByStatusTest.java
@@ -1,21 +1,38 @@
 import io.restassured.response.ValidatableResponse;
-import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.TestData;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import java.util.Arrays;
+import java.util.Collection;
 
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
 public class GetPetByStatusTest {
 
     @Steps
     private PetEndpoint petEndpoint;
     private long createdPetId;
+    private final Status status;
+
+    public GetPetByStatusTest(Status status) {
+        this.status = status;
+    }
+
+    @TestData
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {Status.AVAILABLE},
+                {Status.PENDING},
+                {Status.SOLD},
+        });
+    }
 
     @Before
     public void createPet() {
-        Pet pet = new Pet("0", "sammy", Status.AVAILABLE);
+        Pet pet = new Pet("0", "sammy", status);
         ValidatableResponse response = petEndpoint.createPet(pet);
         createdPetId = response.extract().path("id");
     }
@@ -27,7 +44,6 @@ public class GetPetByStatusTest {
 
     @Test
     public void getPetsByStatus() {
-        Status status = Status.SOLD;
         ValidatableResponse response = petEndpoint.getPetByStatus(status);
     }
 }
--- src/test/java/UploadPetImageTest.java
+++ src/test/java/UploadPetImageTest.java
@@ -1,17 +1,35 @@
 import io.restassured.response.ValidatableResponse;
-import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.TestData;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import java.util.Arrays;
+import java.util.Collection;
 
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
 public class UploadPetImageTest {
 
     @Steps
     private PetEndpoint petEndpoint;
     private long createdPetId;
+    private final String fileName;
+
+    public UploadPetImageTest(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @TestData
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+            {"Cat.jpg"},
+            {"Dog.png"},
+            {"Airbus.tif"},
+            {"Petstore_API_tests.txt"}
+        });
+    }
 
     @Before
     public void createPet() {
@@ -21,7 +39,7 @@ public class UploadPetImageTest {
     }
 
     @Test
-    public void uploadPetImage() {petEndpoint.uploadPetImage(createdPetId, "Cat.jpg");}
+    public void uploadPetImage() {petEndpoint.uploadPetImage(createdPetId, fileName);}
 
     @After
     public void deletePet() {
